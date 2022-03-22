484739faf130cbda2a8f50890814677a920b0651
NEHA ANTONY <93909009+nehaantony2000@users.noreply.github.com>
2022-03-22 12:26:51 +0530

    Add files via upload
    third

---
+++ Sample.java
@@ -0,0 +1,9 @@
+public class Sample
+{  int x=5;
+  public static void main(String[] args)
+  {
+	  System.out.println("hello world");
+	  Sample obj=new Sample();
+	  System.out.println(obj.x);
+  }
+}
