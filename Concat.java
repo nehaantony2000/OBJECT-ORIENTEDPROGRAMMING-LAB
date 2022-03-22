bc9c80aea6f554217f5063682d3e23230f183dd8
NEHA ANTONY <93909009+nehaantony2000@users.noreply.github.com>
2022-03-22 12:26:03 +0530

    Add files via upload
    second

---
+++ Concat.java
@@ -0,0 +1,10 @@
+public class Concat
+{
+   String x="good morning";
+   String y="  welcome to Amal Jyothi  ";
+  public static void main(String[] args)
+  {
+	  Concat obj=new Concat();
+	  System.out.println(obj.x + obj.y);
+  }
+}
