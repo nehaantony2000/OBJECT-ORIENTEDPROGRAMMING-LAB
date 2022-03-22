ffa8c1cab79f459d1d4dc3445be6c51b83ac59bd
NEHA ANTONY <93909009+nehaantony2000@users.noreply.github.com>
2022-03-22 12:25:17 +0530

    Add files via upload
    first

---
+++ Calc.java
@@ -0,0 +1,13 @@
+public class Calc
+{
+  static int x=6;
+  static int y=10;
+  public static void main(String[] args)
+  {
+	
+	  System.out.println("addition=" + x+y);
+	  System.out.println("sub="+ (x-y));
+	  System.out.println("Mul=" + x*y);
+	  System.out.println("Division=" + x/y);
+  }
+}
