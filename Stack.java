0d6eb834df07e00d21a9720da1505a344167175d
NEHA ANTONY <93909009+nehaantony2000@users.noreply.github.com>
2022-06-01 13:04:21 +0530

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,51 @@
+class Stack {
+  private int arr[];
+  private int top;
+  private int N;
+  Stack(int size) {
+    arr = new int[size];
+    N= size;
+    top = -1;
+  }
+  public void push(int x) {
+    if (isFull()) {
+      System.out.println("Stack OverFlow");
+      System.exit(1);
+    }
+    System.out.println("Inserting " + x);
+    arr[++top] = x;
+  }
+  public int pop() {
+    if (isEmpty()) {
+      System.out.println("STACK EMPTY");
+      System.exit(1);
+    }
+    return arr[top--];
+  }
+  public int getSize() {
+    return top + 1;
+  }
+  public Boolean isEmpty() {
+    return top == -1;
+  }
+  public Boolean isFull() {
+    return top == N- 1;
+  }
+  public void printStack() {
+    for (int i = 0; i <= top; i++) {
+      System.out.print(arr[i] + ", ");
+    }
+  }
+  public static void main(String[] args) {
+    Stack stack = new Stack(5);
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    System.out.print("Stack: ");
+    stack.printStack();
+    stack.pop();
+    System.out.println("\nAfter popping out");
+    stack.printStack();
+  }
+}
+
